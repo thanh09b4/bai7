051ce0a6b392b42229a2b30e641452c3311c9653
thanh09b4 <ducthanh294@gmail.com>
2017-03-28 16:22:01 +0700

    internet

---
+++ app/src/main/java/thanh/com/bai7/MainActivity.java
@@ -0,0 +1,71 @@
+package thanh.com.bai7;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                new loadStringURL().execute("http://khoapham.vn/KhoaPhamTraining/android/snipet/");
+            }
+        });
+    }
+    class loadStringURL extends AsyncTask<String, Integer,String>{
+
+        @Override
+        protected String doInBackground(String... params) {
+            String ndung = docNoiDung_Tu_URL(params[0]);
+
+            return ndung;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+        }
+    }
+    private static String docNoiDung_Tu_URL(String theUrl)
+    {
+        StringBuilder content = new StringBuilder();
+
+        try
+        {
+            // create a url object
+            URL url = new URL(theUrl);
+
+            // create a urlconnection object
+            URLConnection urlConnection = url.openConnection();
+
+            // wrap the urlconnection in a bufferedreader
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+            String line;
+
+            // read from the urlconnection via the bufferedreader
+            while ((line = bufferedReader.readLine()) != null)
+            {
+                content.append(line + "\n");
+            }
+            bufferedReader.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        return content.toString();
+    }
+}
